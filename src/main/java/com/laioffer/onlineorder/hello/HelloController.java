be417ca02e7ed06b2ac0641f5a873b2231f2228a
Amy <chiangyenhui@gmail.com>
2025-04-05 16:14:13 -0700

    create docker database, add jdbc and postgresql dependency, init database, update application.yml

---
+++ src/main/java/com/laioffer/onlineorder/hello/HelloController.java
@@ -0,0 +1,33 @@
+package com.laioffer.onlineorder.hello;
+
+
+import net.datafaker.Faker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public Person sayHello(@RequestParam(required = false, defaultValue = "quest"
+    ) String name) {
+
+
+        Faker faker = new Faker();
+        String company = faker.company().name();
+        String street = faker.address().streetAddress();
+        String city = faker.address().city();
+        String country = faker.address().country();
+        String state = faker.address().state();
+        String bookTitle = faker.book().title();
+        String bookAuthor = faker.book().author();
+        return new Person(
+                name, company,
+                new Address(street, city,state,null),
+                new Book(bookTitle, bookAuthor)
+
+        );
+
+    }
+}
